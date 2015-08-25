8636d59478a637a5dba09d5fe263e8d59af9fe20
Andrew Connell <me@andrewconnell.com>
2015-08-25 07:47:18 -0400

    updated npm global packages

--- scripts/npminstall.sh
+++ scripts/npminstall.sh
@@ -1,26 +1,31 @@
 #!/bin/sh
 
+# utilities
 npm install -g azure-cli
+npm install -g growl
+npm install -g http-server
+npm install -g kudoexec
+npm install -g nodetree
+npm install -g superstatic
+
+# developer utils
 npm install -g bower
 npm install -g express
-npm install -g growl
 npm install -g grunt-cli
 npm install -g gulp
-npm install -g http-server
+npm install -g istanbul
 npm install -g jspm
 npm install -g karma-cli
-npm install -g kudoexec
 npm install -g mocha
 npm install -g node-inspector
 npm install -g nodemon
-npm install -g nodetree
 npm install -g phantomjs
-npm install -g superstatic
 npm install -g systemjs
 npm install -g tsd
 npm install -g typescript
-npm install -g yo
 
+# yeoman generators
+npm install -g yo
 npm install -g generator-angular
 npm install -g generator-common
 npm install -g generator-generator
