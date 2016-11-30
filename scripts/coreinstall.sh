559fe0c32e663d9b1bfdf4c0c431cfa46dfcfec8
Andrew Connell <me@andrewconnell.com>
2016-11-30 17:38:38 -0500

    updated brew installs

--- scripts/coreinstall.sh
+++ scripts/coreinstall.sh
@@ -4,6 +4,7 @@ brew cask install alfred
 brew cask install bartender
 brew cask install cheatsheet
 brew cask install crashplan
+brew cask install cuda
 brew cask install dropbox
 brew cask install duet
 brew cask install joinme
@@ -11,12 +12,18 @@ brew cask install google-chrome
 brew cask install google-drive
 brew cask install iterm2
 brew cask install nosleep
+brew cask install openssl
+brew cask install pkg-config
+brew cask install python3
+brew cask install readline
 brew cask install skype
 brew cask install slack
+brew cask install sqlite
 brew cask install steam
 brew cask install sublime-text
 brew cask install tripmode
 brew cask install vlc
+brew cask install xz
 
 brew install bash-completion
 brew tap homebrew/completions
--- scripts/npminstall.sh
+++ scripts/npminstall.sh
@@ -1,28 +1,23 @@
 #!/bin/sh
-
-# utilities
+npm install -g microsoft/generator-sharepoint
 npm install -g azure-cli
-npm install -g growl
-npm install -g http-server
-npm install -g nodetree
-npm install -g superstatic
-
-# developer utils
 npm install -g bower
 npm install -g browserify
 npm install -g express
 npm install -g grunt-cli
 npm install -g gulp
+npm install -g http-server
 npm install -g istanbul
 npm install -g jshint
 npm install -g jspm
 npm install -g karma-cli
 npm install -g mocha
-npm install -g node-inspector
 npm install -g nodemon
+npm install -g nodetree
+npm install -g npm
 npm install -g phantomjs
-npm install -g systemjs
-npm install -g typescript
+npm install -g superstatic
 npm install -g typings
 npm install -g webpack
 npm install -g yo
+
