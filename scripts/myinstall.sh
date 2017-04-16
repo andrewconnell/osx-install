4cdd4baeaa82e43d005e2440c4d6b0b0202c6d03
Andrew Connell <me@andrewconnell.com>
2017-04-16 06:01:37 -0400

    add dvd & bluray ripping

--- scripts/myinstall.sh
+++ scripts/myinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 brew cask install gopro-studio
-brew cask install handbrake
 brew cask install logitech-harmony
 brew cask install sketchup
 brew cask install synergy
@@ -22,3 +21,12 @@ brew cask install qlmarkdown
 brew cask install qlstephen
 brew cask install quicklook-csv
 brew cask install quicklook-json
+
+# ripping dvds & blurays for PERSONAL archive
+brew cask install handbrake
+brew install libdvdcss
+brew cask install makemkv
+# configure handbrake to use makemkv for bluray
+mkdir -p ~/lib
+ln -s /Applications/MakeMKV.app/Contents/lib/libmmbd.dylib ~/lib/libaacs.dylib
+ln -s /Applications/MakeMKV.app/Contents/lib/libmmbd.dylib ~/lib/libbdplus.dylib
