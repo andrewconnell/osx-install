67fb52a4706aa79d2e833e4353dff25ee98c016f
Andrew Connell <me@andrewconnell.com>
2017-04-28 06:29:40 -0400

    update vscode & npm install list

--- scripts/npminstall.sh
+++ scripts/npminstall.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+npm install -g @angular/cli
 npm install -g @microsoft/generator-sharepoint
 npm install -g azure-cli
 npm install -g bower
@@ -14,11 +15,12 @@ npm install -g jspm
 npm install -g karma-cli
 npm install -g mocha
 npm install -g nodemon
+npm install -g node-sass
 npm install -g nodetree
 npm install -g phantomjs
+npm install -g serverless
 npm install -g source-map-explorer
 npm install -g superstatic
 npm install -g typings
 npm install -g webpack
 npm install -g yo
-
