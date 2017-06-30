1e246077664169914b11b7aff4ae926a0e098969
Andrew Connell <me@andrewconnell.com>
2017-06-30 16:22:55 -0400

    fix typo in macos install

--- scripts/macos-install.sh
+++ scripts/macos-install.sh
@@ -9,14 +9,14 @@ brew install mas
 mas signin $APPLEID
 
 # install macos apps
-mas instasll 937984704 # Amphetamine
-mas instasll 603117688 # CCMenu
-mas instasll 549083868 # Display Menu
-mas instasll 715464874 # Disk Map
-mas instasll 413857545 # Divvy
-mas instasll 524141863 # Jump Desktop
-mas instasll 405399194 # Kindle
-mas instasll 638332853 # Logitech Camera Settings
-mas instasll 639968404 # Parcel
-mas instasll 548125767 # WeathrClip
-mas instasll 410628904 # Wunderlist
+mas install 937984704 # Amphetamine
+mas install 603117688 # CCMenu
+mas install 549083868 # Display Menu
+mas install 715464874 # Disk Map
+mas install 413857545 # Divvy
+mas install 524141863 # Jump Desktop
+mas install 405399194 # Kindle
+mas install 638332853 # Logitech Camera Settings
+mas install 639968404 # Parcel
+mas install 548125767 # WeathrClip
+mas install 410628904 # Wunderlist
