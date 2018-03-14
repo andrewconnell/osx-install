b8a5fdc858dabbbc20e75980ed5185fbf971a969
Andrew Connell <me@andrewconnell.com>
2018-03-14 05:26:55 -0400

    update globally installed NPM packages

--- scripts/npm-global-package-install.sh
+++ scripts/npm-global-package-install.sh
@@ -4,6 +4,8 @@ npm install -g @angular/cli
 npm install -g @microsoft/generator-sharepoint
 npm install -g @pnp/office365-cli
 npm install -g eslint
+npm install -g express
+npm install -g grunt-cli
 npm install -g gulp
 npm install -g h2m
 npm install -g http-server
@@ -14,11 +16,11 @@ npm install -g keycred
 npm install -g mocha
 npm install -g nodemon
 npm install -g nodetree
+npm install -g npm
+npm install -g removeNPMAbsolutePaths
 npm install -g source-map-explorer
+npm install -g sql-cli
 npm install -g systemjs
 npm install -g tslint
-npm install -g typedoc
 npm install -g typescript
-npm install -g uglify-js
-npm install -g webpack
 npm install -g yo
