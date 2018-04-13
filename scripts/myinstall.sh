1dd1195bd61850550f5f52e6436af547c1b707da
Andrew Connell <me@andrewconnell.com>
2018-04-13 05:51:14 -0400

    added MSFT Segoe and Fabric fonts

--- scripts/myinstall.sh
+++ scripts/myinstall.sh
@@ -34,3 +34,15 @@ brew cask install makemkv
 mkdir -p ~/lib
 ln -s /Applications/MakeMKV.app/Contents/lib/libmmbd.dylib ~/lib/libaacs.dylib
 ln -s /Applications/MakeMKV.app/Contents/lib/libmmbd.dylib ~/lib/libbdplus.dylib
+
+# install fonts
+cd ~/Library/Fonts
+MSFT_FONTS="Segoe UI and Fabric MDL2"
+mkdir "$MSFT_FONTS"
+cd "$MSFT_FONTS"
+curl -O "https://github.com/andrewconnell/osx-install/raw/master/app-settings/fonts/Segoe UI and Fabric MDL2/FabExMDL2.ttf"
+curl -O "https://github.com/andrewconnell/osx-install/raw/master/app-settings/fonts/Segoe UI and Fabric MDL2/segoeui.ttf"
+curl -O "https://github.com/andrewconnell/osx-install/raw/master/app-settings/fonts/Segoe UI and Fabric MDL2/segoeuib.ttf"
+curl -O "https://github.com/andrewconnell/osx-install/raw/master/app-settings/fonts/Segoe UI and Fabric MDL2/segoeuil.ttf"
+curl -O "https://github.com/andrewconnell/osx-install/raw/master/app-settings/fonts/Segoe UI and Fabric MDL2/segoeuisl.ttf"
+curl -O "https://github.com/andrewconnell/osx-install/raw/master/app-settings/fonts/Segoe UI and Fabric MDL2/seguisb.ttf"
