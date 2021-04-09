b968d39abc1a1de6f071a9f4f14b88b00e60eec6
Esteban Loboguerrero <e@lovozeto.com>
2021-04-08 20:17:42 -0500

    Revert "Updated old / non-working scripts"
    This reverts commit fc2c94981f7fa597e467dce3292f7c0a03f35f14.

--- scripts/homebrew-setup.sh
+++ scripts/homebrew-setup.sh
@@ -2,8 +2,9 @@
 
 # add additional repos for homebrew to track, update & install
 echo "Starting setup..."
-brew tap homebrew/cask-drivers
-brew tap homebrew/cask-fonts
+brew tap cask
+brew tap cask-drivers
+brew tap cask-fonts
 
 # install packages
 PACKAGES=(
