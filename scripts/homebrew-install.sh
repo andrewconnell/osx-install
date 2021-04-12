24d0249b74ad7ff2ea04267fb1d20ea7ca08b32a
Esteban Loboguerrero <accounts@lovozeto.com>
2021-04-12 10:17:47 -0700

    Update homebrew-install.sh

--- scripts/homebrew-install.sh
+++ scripts/homebrew-install.sh
@@ -3,7 +3,7 @@
 # Check for Homebrew, install if we don't have it
 if test ! $(which brew); then
     echo "Installing homebrew..."
-    curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 brew doctor
