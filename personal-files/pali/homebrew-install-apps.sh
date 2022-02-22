278c6a866853709eb917b26476436a0dd91ba153
Esteban Loboguerrero <accounts@lovozeto.com>
2022-02-21 21:31:15 -0500

    Fixed Esteban, added Pali

--- personal-files/pali/homebrew-install-apps.sh
+++ personal-files/pali/homebrew-install-apps.sh
@@ -3,19 +3,8 @@
 # install cask apps
 CASKS=(
     appcleaner
-    balenaetcher
-    figma-beta
     keka
-    logitech-options
-    #makemkv
-    #microsoft-edge
     onyx
-    sf-symbols
-    #sketch
-    surfshark
-    unpkg
-    visual-studio-code-insiders
-    sf-symbols
 )
 
 echo "Installing apps from the web..."
@@ -32,14 +21,9 @@ mas signin $APPLEID
 # install macos apps
 APPS=(
     1440147259 # Adguard-for-safari
-    1591303229 # Vinegar
-    1601151613 # Baking Soda
-    1024974133 # Mountain Duck
-    803453959 # Slack
     409183694 # Keynote
     409201541 # Pages
     409203825 # Numbers
-    1351639930 # Gifski
 )
 
 echo "Installing apps from the App Store..."
