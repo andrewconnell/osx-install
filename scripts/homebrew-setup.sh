a625d09b54fded1c80c9f43b8870ea9499eb9150
Esteban Loboguerrero <accounts@lovozeto.com>
2022-02-21 23:02:54 -0500

    Update homebrew-setup.sh

--- scripts/homebrew-setup.sh
+++ scripts/homebrew-setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Add Homebrew to PATH
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/ralphjsmit/.zprofile
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew doctor
 
