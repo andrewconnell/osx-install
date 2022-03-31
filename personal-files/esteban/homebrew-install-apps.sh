5893ae9f2258be69fd1834b2c716d0a68bd7f2b6
Esteban Loboguerrero <accounts@lovozeto.com>
2022-03-31 01:05:01 -0400

    Restored Teban

--- personal-files/esteban/homebrew-install-apps.sh
+++ personal-files/esteban/homebrew-install-apps.sh
@@ -40,17 +40,12 @@ mas signin $APPLEID
 # install macos apps
 APPS=(
     1440147259 # Adguard - Safari
-    1591303229 # Vinegar - Safari
-    1601151613 # Baking Soda - Safari
     1564329434 # Momentum - Safari
     1024974133 # Mountain Duck
     803453959 # Slack
-    693112260 # Sim Daltonism
-    493145008 # Headspace
     409183694 # Keynote
     409201541 # Pages
     409203825 # Numbers
-    1351639930 # Gifski
 )
 
 echo "Installing apps from the App Store..."
