d13049061f2464b4c0c489ab3ac11ca2a3e31ad9
Andrew Connell <me@andrewconnell.com>
2025-01-04 06:57:26 -0500

    add chatgpt

--- scripts/homebrew-install-apps.zsh
+++ scripts/homebrew-install-apps.zsh
@@ -51,6 +51,7 @@ brew install --cask bartender
 brew install --cask bitbar
 brew install --cask bot-framework-emulator
 brew install --cask canva
+brew install --cask chatgpt
 brew install --cask discord
 brew install --cask docker
 brew install --cask dotnet-sdk
